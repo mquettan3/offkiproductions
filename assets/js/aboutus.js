18ce02b080f3bd1a84f6ddb490af2949efeab900
Marcus Quettan <mquettan@gmail.com>
2019-05-19 10:29:39 -0400

    Adding about us page.

---
+++ assets/js/aboutus.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+    /* ======= ScrollTo ======= */
+    $('a.scrollto').on('click', function(e){
+
+        //store hash
+        var target = this.hash;
+
+        e.preventDefault();
+
+				$('body').scrollTo(target, 800, {offset: -50, 'axis':'y'});
+		        //Collapse mobile menu after clicking
+				if ($('.navbar-collapse').hasClass('show')){
+					$('.navbar-collapse').removeClass('show');
+				}
+
+		});
+
+});
